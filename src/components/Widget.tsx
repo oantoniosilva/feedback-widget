28ec54a681153fc13ddc2c266ae8c4f3cd1a4604
antonio-silva <antoniopsilva083@gmail.com>
2022-05-08 03:03:03 -0300

    feat: create widgetform

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,25 +1,47 @@
 import { ChatTeardropDots} from 'phosphor-react'
-import { useState } from 'react'
+import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm'
 
 export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false)
-
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen)
-  }
-
   return (
-    <div className='absolute bottom-5 right-5'>
-      { isWidgetOpen && <p>Hello World</p> }
+    <Popover 
+      className='absolute 
+      bottom-4 
+      right-4 
+      md:botton-10 
+      md:right-10 
+      flex 
+      flex-col 
+      items-end'
+    >
+      <Popover.Panel>
+        <WidgetForm />
+      </Popover.Panel>
 
-      <button onClick={toggleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+      <Popover.Button 
+        className='bg-brand-500 
+        rounded-full 
+        px-3 
+        h-12 
+        text-white 
+        flex 
+        items-center 
+        group'
+      >
         <ChatTeardropDots className='w-6 h-6'/>
 
-        <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
+        <span 
+          className='max-w-0 
+          overflow-hidden 
+          group-hover:max-w-xs 
+          transition-all 
+          duration-500 
+          ease-linear'
+        >
           <span className='pl-2'></span>
           Feedback
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   )
 }
