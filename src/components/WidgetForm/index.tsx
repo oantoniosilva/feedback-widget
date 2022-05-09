7f4387bc36817b890dba2f375649e495b81a4145
antonio-silva <antoniopsilva083@gmail.com>
2022-05-09 00:45:20 -0300

    feat: create feedbackTypeStep

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,10 +1,12 @@
-import { CloseButton } from "./CloseButton"
+import { useState } from "react";
+import { CloseButton } from "../CloseButton"
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 
-import bugImageUrl from '../assets/bug.svg'
-import ideaImageUrl from '../assets/idea.svg'
-import thoughtImageUrl from '../assets/thought.svg'
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: 'Problema',
     image: {
@@ -37,7 +39,11 @@ const feedbackTypes = {
  * ]
  */
 
+export type FeedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lgw-[calc(100vw-2rem)]  md:w-auto">    
     <header>
@@ -46,21 +52,11 @@ export function WidgetForm() {
       <CloseButton />
     </header>
       
-    <div className="flex py-8 gap-2 w-full">
-      { Object.entries(feedbackTypes).map(([key, value]) => {
-        console.log(key, value);
-        return (
-          <button
-          key={key}
-            className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-            type="button"
-          >
-            <img src={value.image.source} alt={value.image.alt} />
-            <span>{value.title}</span>
-          </button>
-        );
-      }) }
-    </div>
+    {!feedbackType ? (
+      <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+    ) : (
+      <p>Hello World!</p>
+    )}
 
     <footer className='text-xs text-neutral-400'>
       Feito com ♥ pela <a className='underline underline-offset-2' href='https://rocketseat.com.br'>Rocketseat</a>
